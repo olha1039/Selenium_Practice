b7be415c07dd5e82113035ae045bef64727f5ec7
olga1039 <olga-d7@outlook.com>
2022-06-13 11:30:24 -0400

    started US2

--- src/test/java/com/cydeo/test/CRM/CRM_US2.java
+++ src/test/java/com/cydeo/test/CRM/CRM_US2.java
@@ -8,53 +8,34 @@ import org.testng.annotations.Test;
 
 public class CRM_US2 extends TestBase{
 
-    // Description:  Users send text messages successfully
-//1.  Users are on the homepage (Login with valid username and password)
-//2.  Users click the MESSAGE tab
-//3.  Users write test message
-//4.  Users click the SEND button
-//5.  Verify the message is displayed on the feed
-
-
+    // Given user successfully logged in to CRM page
+    // When user click on message button
+    // And user types message in the message console
+    // And user clicks send button
+    // The message should be displayed in the feed
     @Test
-    public void sendText(){
+    public void AC1sendText(){
         driver.get("https://login2.nextbasecrm.com/");
-
-//        3- Login Homepage ( Login with valid username and password)
-
-        CRMLoginUtil.crm_login(driver, "helpdesk57@cybertekschool.com", "UserUser");
-//        4- Click the MORE tab and select APPRECIATION
-
-        WebElement moreBtn = driver.findElement(By.cssSelector("span[id*='link-text']")); //xpath "//span[contains(@id,'link-text-)]"
-        moreBtn.click();
-
-//        5- Write an Appreciation message
-
-        WebElement appreciation = driver.findElement(By.xpath("//span[.='Appreciation']"));
-        appreciation.click();
-
-//        6- Click the SEND button
-
+        CRMLoginUtil.crm_login(driver, "marketing57@cybertekschool.com", "UserUser");
+        WebElement messageBtn = driver.findElement(By.cssSelector("span[id*='form-tab-message']"));
+        messageBtn.click();
         driver.switchTo().frame(driver.findElement(By.cssSelector(".bx-editor-iframe")));
-        driver.findElement(By.tagName("body")).sendKeys("Hello from the other side!");
-
-        // if you have nested frame going one frame up will be parent frame, otherwise default is main html frame
-
+        driver.findElement(By.tagName("body")).sendKeys("Test");
         driver.switchTo().defaultContent();
         WebElement sendBtn = driver.findElement(By.id("blog-submit-button-save"));
         sendBtn.click();
 
-//        7- Verify the Appreciation message is displayed on the feed
-
         WebElement feedText = driver.findElement(By.xpath("//div[starts-with(@id, 'blog_post_body')]"));
         String actualText = feedText.getText();
-        String expectedText = "Hello from the other side!";
+        String expectedText = "Test";
         Assert.assertEquals(actualText,expectedText,"Feed message did not appear correctly!");
-
-
-
-
     }
+
+
+
+
+
+
     }
 
 
